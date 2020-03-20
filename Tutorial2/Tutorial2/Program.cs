4029611685446de76fdd7f5ce611b2e3bb610208
Jack <Jack@GERALT>
2020-03-20 08:54:27 +0100

    tut2

---
+++ Tutorial2/Tutorial2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Tutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"Data\dane.csv";
+
+            //Reading from file
+            var fi = new FileInfo(path);
+
+        }
+    }
+}
